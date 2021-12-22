535f2849fc3ffe104d0ee2b12d45421f23b62f77
Yoonjungho <wjdgh8926@naver.com>
2021-12-23 02:05:50 +0900

    dd
    dd

---
+++ python/module/test_module.py
@@ -0,0 +1,12 @@
+class Hello:
+    def __init__(self,name):
+        self.name = name
+
+    def hello(self):
+        print("hello {} !!".format(self.name))
+
+
+def world():
+    print("hello world")
+
+feeling = "tired"
---
+++ python/str/main.py
@@ -0,0 +1,12 @@
+
+class Hello :
+    def __str__(self):
+        return "헬로클래스입니돵~!"
+
+
+
+cls1= Hello()
+
+print(cls1)
+
+
