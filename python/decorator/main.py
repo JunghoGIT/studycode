51282ec8c4e0cb5afb2193c83df1541bdd64f879
Yoonjungho <wjdgh8926@naver.com>
2022-02-28 00:11:07 +0900

    Update main.py

--- python/decorator/main.py
+++ python/decorator/main.py
@@ -14,4 +14,33 @@ def test():
     print("함수 실행")
     math.factorial(100000)
 
+
+class TimerDeco:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self):
+        print('속도 측정 시작')
+        start = time.time()
+        self.func()
+        end = time.time()
+        print(f"속도 측정 결과 {end - start}")
+
+class Test:
+
+    @TimerDeco
+    def test():
+        print("함수 실행")
+        math.factorial(100000)
+
+    @TimerDeco
+    def test_2():
+        print("함수 실행")
+        math.factorial(200000)
+
+
 test()
+
+t = Test
+t.test()
+t.test_2()
